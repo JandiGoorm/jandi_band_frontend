dbcbbe4428f3fe82e2742ceb106c6998a61e5757
S2ej1n <ks320205@naver.com>
2025-05-16 02:50:17 +0900

    Design: 유튜브 썸네일과 유저 정보

--- src/pages/vote/VoteSongCard.tsx
+++ src/pages/vote/VoteSongCard.tsx
@@ -1,21 +1,23 @@
 import styles from "./style/VoteSongCard.module.css";
+import profile from "./style/profile.svg";
 
 const SongCard = () => {
   return (
     <article className={styles.song_card}>
-      <header>
+      <header className={styles.song_title}>
         <h2>
           터치드 - <span>Hi Bully</span>
         </h2>
       </header>
 
-      <figure>유튜브 링크 썸네일</figure>
+      <figure className={styles.song_youtube}>유튜브 링크 썸네일</figure>
 
-      <section>
+      <section className={styles.vote_section}>
         <div className={styles.user}>
-          <p>img 프로필사진</p>
+          <img src={profile} />
           <span>강세진</span>
         </div>
+
         <div className={styles.vote_emoji}>
           <button>🙆‍♀️</button>
           <span>10</span>
