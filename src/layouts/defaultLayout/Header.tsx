d9273d2c8d8ea220915bd3bb0e30abe64de619ac
Yoonhwi <ush0105@naver.com>
2025-05-15 15:59:47 +0900

    style: 로고가 변경됨에 따라 DefaulyLayout의 Header CSS 미세 조정

--- src/layouts/defaultLayout/Header.tsx
+++ src/layouts/defaultLayout/Header.tsx
@@ -4,9 +4,7 @@ const Header = () => {
   return (
     <div className={styles.container}>
       <div className={styles.content}>
-        <div className={styles.logo}>
-          <img src={"/logo.png"} alt="logo" />
-        </div>
+        <img src={"/logo.png"} alt="logo" className={styles.logo} />
       </div>
     </div>
   );
