88291d873a0dcafd5da3b0951144ebdf629f54a7
S2ej1n <ks320205@naver.com>
2025-05-16 19:21:36 +0900

    design: 화살표 추가

--- src/pages/vote/result/VoteResult.tsx
+++ src/pages/vote/result/VoteResult.tsx
@@ -1,9 +1,19 @@
 import DefaultLayout from "@/layouts/defaultLayout/DefaultLayout";
+import ArrowBack from "@/pages/vote/style/arrowback.svg";
+import styles from "@/pages/vote/style/VoteResult.module.css";
 
 const VoteResult = () => {
   return (
     <DefaultLayout>
-      <main>결과페이지</main>
+      <main className={styles.vote_result_container}>
+        <header className={styles.header}>
+          <section className={styles.vote_title}>
+            <img src={ArrowBack} alt="뒤로가기" />
+            <h1>5월 대동제 곡 투표 결과</h1>
+          </section>
+          <section>뻐튼</section>
+        </header>
+      </main>
     </DefaultLayout>
   );
 };
