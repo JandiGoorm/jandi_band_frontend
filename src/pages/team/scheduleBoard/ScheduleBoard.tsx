ad57a8174003978d8d0ba21d439256636ce1cda7
Yoonhwi <ush0105@naver.com>
2025-05-19 16:11:48 +0900

    feat: 연습일정 UI 구현

---
+++ src/pages/team/scheduleBoard/ScheduleBoard.tsx
@@ -0,0 +1,27 @@
+import styles from "./ScheduleBoard.module.css";
+import { FiPlus } from "react-icons/fi";
+import { dummyTeam } from "../constants";
+
+const ScheduleBoard = () => {
+  return (
+    <section className={styles.container}>
+      <header className={styles.header}>
+        <h2>팀 연습 일정</h2>
+        <button className={styles.header_button}>
+          <FiPlus size={20} />
+        </button>
+      </header>
+
+      <div className={styles.content_container}>
+        {dummyTeam.practiceTime.map((time) => (
+          <div className={styles.content_item} key={time.id}>
+            <span className={styles.content_type}>{time.type}</span>
+            <span>{time.time}</span>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default ScheduleBoard;
