327c72b1ca1864069474ebe28b5ffa245f5276bb
JunMinGeun <80248856+Jun-min-geun@users.noreply.github.com>
2025-05-20 19:10:34 +0900

    feat: 일정 등록 모달 구현

--- src/pages/club/main/clubCalendar/ClubCalendar.tsx
+++ src/pages/club/main/clubCalendar/ClubCalendar.tsx
@@ -1,14 +1,23 @@
 import Button from "@/components/button/Button";
 import styles from "./ClubCalendar.module.css";
 import Calendar from "./calendar/Calendar";
+import Modal from "@/components/modal/Modal";
+import ScheduleModal from "./ScheduleModal";
 
 const ClubCalendar = () => {
   return (
     <main className={styles.container}>
       <header className={styles.header}>
-        <Button variant="primary" size="md">
-          일정 추가
-        </Button>
+        <Modal
+          title="일정 추가하기"
+          trigger={
+            <Button variant="primary" size="md">
+              일정 추가
+            </Button>
+          }
+        >
+          <ScheduleModal />
+        </Modal>
       </header>
       <Calendar />
     </main>
