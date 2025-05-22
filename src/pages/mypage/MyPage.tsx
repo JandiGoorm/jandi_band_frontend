b308364369edeb2b34fb36f6fc9ccc8068777b36
S2ej1n <ks320205@naver.com>
2025-05-22 20:59:46 +0900

    design: 마이페이지 음표 추가

--- src/pages/mypage/MyPage.tsx
+++ src/pages/mypage/MyPage.tsx
@@ -3,6 +3,8 @@ import styles from "@/pages/mypage/MyPage.module.css";
 import Profile from "@/pages/vote/style/profile.svg";
 import Slide from "@/components/slide/Slide";
 import Button from "@/components/button/Button";
+import MusicNote1 from "/public/musical_note1.svg";
+import MusicNote2 from "/public/musical_notes2.svg";
 
 const MyPage = () => {
   const sampleItems = [
@@ -19,7 +21,10 @@ const MyPage = () => {
       <main className={styles.mypage_container}>
         <section className={styles.myprofile_box}>
           <header className={styles.profile_title}>
-            <h2>마이 프로필</h2>
+            <div>
+              <img src={MusicNote1} alt="음표" />
+              <h2>마이 프로필</h2>
+            </div>
             <button>수정하기</button>
           </header>
           <div className={styles.profile_content}>
@@ -47,7 +52,10 @@ const MyPage = () => {
         </section>
 
         <section className={styles.team_box}>
-          <h2>참여 팀 목록</h2>
+          <div>
+            <img src={MusicNote2} alt="음표" />
+            <h2>참여 팀 목록</h2>
+          </div>
           <Slide items={sampleItems} size="sm">
             {(item) => (
               <div className={styles.itemCard}>
@@ -60,7 +68,10 @@ const MyPage = () => {
 
         <section className={styles.timetable_box}>
           <header className={styles.timetable_title}>
-            <h2>내 시간표 관리</h2>
+            <div>
+              <img src={MusicNote1} alt="음표" />
+              <h2>내 시간표 관리</h2>
+            </div>
             <Button variant="primary" className={styles.timetable_button}>
               시간표 추가
             </Button>
