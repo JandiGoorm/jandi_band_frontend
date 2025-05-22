b218df22bd9fffb0410e3e0c28594a4c8bb30aa7
Junmingeun <junminguen@naver.com>
2025-05-22 18:54:00 +0900

    feat: pagenation 공통 타입 지정

--- src/types/common.ts
+++ src/types/common.ts
@@ -1,1 +1,10 @@
 export type Nullable<T> = T | null;
+
+export interface PageNationResponse<T> {
+  content: T[];
+  page: number;
+  size: number;
+  totalElements: number;
+  totalPages: number;
+  last: boolean;
+}
