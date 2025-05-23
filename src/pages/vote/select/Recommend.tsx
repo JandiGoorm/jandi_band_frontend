1b71ea5dcdfcac2624685559eee2e34862ae1e5a
S2ej1n <ks320205@naver.com>
2025-05-24 02:21:43 +0900

    feat: 코드 수정 (불필요한  코드 제거)

--- src/pages/vote/select/Recommend.tsx
+++ src/pages/vote/select/Recommend.tsx
@@ -1,34 +1,17 @@
-import { useForm } from "react-hook-form";
-import { z } from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
+// import { useForm } from "react-hook-form";
+// import { z } from "zod";
+// import { zodResolver } from "@hookform/resolvers/zod";
 import styles from "@/pages/vote/select/Recommend.module.css";
 import Input from "@/components/input/Input";
 import Button from "@/components/button/Button";
 
 const Recommend = () => {
-  const formSchema = z.object({
-    name: z.string().nonempty("이름은 필수입니다"),
-  });
-
-  // 이 객체 안에 register가 들어있음.
-  const form = useForm({
-    resolver: zodResolver(formSchema),
-  });
-
   return (
     <main className={styles.recommend_container}>
       <p>
         <span>*</span> 는 필수 입력 항목입니다.
       </p>
-      <form
-        className={styles.form_container}
-        onSubmit={form.handleSubmit((data) => console.log(data))}
-      >
-        <input {...form.register("name")} />
-        {form.formState.errors.name && (
-          <p>{form.formState.errors.name.message}</p>
-        )}
-
+      <form className={styles.form_container}>
         <Input inputSize="sm" />
         <Input inputSize="sm" />
         <Input inputSize="sm" />
