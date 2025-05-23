06b3928594bebdb0582225e8d6662ff437a583f7
S2ej1n <ks320205@naver.com>
2025-05-23 17:00:38 +0900

    design: 필수 입력 항목

--- src/pages/vote/select/Recommend.tsx
+++ src/pages/vote/select/Recommend.tsx
@@ -1,9 +1,12 @@
+import styles from "@/pages/vote/select/Recommend.module.css";
+
 const Recommend = () => {
   return (
-    <main>
+    <main className={styles.recommend_container}>
       <p>
         <span>*</span> 는 필수 입력 항목입니다.
       </p>
+      <form></form>
     </main>
   );
 };
