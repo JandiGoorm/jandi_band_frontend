33ae4d357523084b1579ea31cc625eb21a60fd9d
Yoonhwi <ush0105@naver.com>
2025-05-24 12:42:35 +0900

    feat: Field컴포넌트 필수값 표시를 위해 isRequired 추가

--- src/components/field/Field.tsx
+++ src/components/field/Field.tsx
@@ -8,6 +8,7 @@ interface FieldProps extends React.HTMLAttributes<HTMLDivElement> {
   error: FieldError | undefined; // react-hook-form 에러 객체
   helperText?: string | undefined; // input작성 시 도움 텍스트 ex) 3~10자 특수문자를 포함해서 입력해주세요.
   className?: string; // 필드 컴포넌트 추가 클래스
+  isRequired?: boolean; // 필수 필드 여부
 }
 
 const Field = ({
@@ -16,12 +17,16 @@ const Field = ({
   error,
   helperText,
   className,
+  isRequired = false,
   ...props
 }: FieldProps) => {
   return (
     <div className={clsx(styles.container, className)} {...props}>
       <div className={styles.label_container}>
-        <label className={styles.label}>{label}</label>
+        <label className={styles.label}>
+          {isRequired && <span className={styles.required}>*</span>}
+          {label}
+        </label>
         {helperText && <p className={styles.helper_text}>{helperText}</p>}
       </div>
       {children}
