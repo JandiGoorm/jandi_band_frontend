7a11419d5668d76c3ec3e142bfedf859755e63f7
S2ej1n <ks320205@naver.com>
2025-05-27 17:12:58 +0900

    feat: poll 관련 메소드 vote 파일에 정의

---
+++ src/apis/vote.ts
@@ -0,0 +1,24 @@
+// 곡 투표 관련 api
+// import { ApiEndpotins } from "@/constants/endpoints";
+// import { useDelete, useFetch, usePost } from "./hooks";
+// import { buildPath } from "@/utils/buildPath";
+
+// 투표 상세 조회
+export const useGetPoll = () => {
+  return;
+};
+
+// 투표에 곡 추가
+export const usePostPoll = () => {
+  return;
+};
+
+// 곡에 투표하기
+export const usePutPoll = () => {
+  return;
+};
+
+// 곡 투표 취소
+export const useDeletePoll = () => {
+  return;
+};
--- src/types/vote.ts
+++ src/types/vote.ts
@@ -1,6 +1,24 @@
 // 곡 투표 페이지 타입들 정의
 
-// /api/polls/{pollId}
+// 곡 투표 생성 응답 (곡 투표의 기본 베이스)
+export interface CreatePollType {
+  id: number;
+  title: string;
+  clubId: number;
+  clubName: string;
+  startDatetime: string;
+  endDatetime: string;
+  creatorId: number;
+  creatorName: string;
+  createdAt: string;
+}
+
+// 곡 투표 상세 조회 /polls/{pollId}
+export interface PollDetailType extends CreatePollType {
+  songs: SongType[];
+}
+
+// 곡 자체의 타입들 (곡 추가, 곡 투표, 곡 투표 취소)
 export interface SongType {
   id: number;
   pollId: number;
