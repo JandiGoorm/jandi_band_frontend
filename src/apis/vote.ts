4e57daf6618d41481fa24d1a8b37a4039b6f11e7
S2ej1n <ks320205@naver.com>
2025-05-27 17:48:30 +0900

    feat: api vote에 T,S 타입 추가

--- src/apis/vote.ts
+++ src/apis/vote.ts
@@ -2,25 +2,30 @@
 import { ApiEndpotins } from "@/constants/endpoints";
 import { useDelete, useFetch, useUpdate, usePost } from "./hooks";
 import { buildPath } from "@/utils/buildPath";
+import type { PollDetailType, SongType } from "@/types/vote";
 
-// 투표 상세 조회 GET
+// 투표 상세 조회 GET - 응답형식 PollDetailType
 export const useGetPoll = (pollId: string) => {
-  return useFetch(buildPath(ApiEndpotins.POLL, { pollId }));
+  return useFetch<PollDetailType>(buildPath(ApiEndpotins.POLL, { pollId }));
 };
 
 // 투표에 곡 추가 POST
+// 곡 추가 타입 any에 적어야함. (임시방편으로 해놨음.)
 export const usePostPoll = (pollId: string) => {
-  return usePost(buildPath(ApiEndpotins.POLL_ADD_RECOMMEND, { pollId }));
+  return usePost<undefined, SongType>(
+    buildPath(ApiEndpotins.POLL_ADD_RECOMMEND, { pollId })
+  );
 };
 
 // 곡에 투표하기 PUT
 export const usePutPoll = (pollId: string, songId: string, emoji: string) => {
-  return useUpdate(
+  return useUpdate<undefined, SongType>(
     buildPath(ApiEndpotins.POLL_VOTE, { pollId, songId, emoji })
   );
 };
 
 // 곡 투표 취소 DELETE
+// 여기는 승휘님께 물어봐야할듯. 타입 정의 바꿔도되는지!
 export const useDeletePoll = (
   pollId: string,
   songId: string,
