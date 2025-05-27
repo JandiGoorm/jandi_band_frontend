845af02fee5bede9d609123d76c5ae9c6188ee27
S2ej1n <ks320205@naver.com>
2025-05-28 02:30:47 +0900

    feat: 결과 데이터 연결

--- src/pages/vote/result/VoteResult.tsx
+++ src/pages/vote/result/VoteResult.tsx
@@ -1,12 +1,25 @@
 import DefaultLayout from "@/layouts/defaultLayout/DefaultLayout";
 import BarChart from "../graph/BarChart";
-import voteData from "@/pages/vote/graph/voteData";
 import { useNavigate } from "react-router-dom";
 import ArrowBack from "@/pages/vote/style/arrowback.svg";
 import styles from "@/pages/vote/result/VoteResult.module.css";
+import { usePollStore } from "@/stores/voteStore";
+import Loading from "@/components/loading/Loading";
 
 const VoteResult = () => {
   const navigate = useNavigate();
+  const { poll } = usePollStore();
+
+  const voteData =
+    poll?.songs.map((song) => ({
+      song: song.songName,
+      좋아요: song.likeCount,
+      별로예요: song.dislikeCount,
+      실력부족: song.cantCount,
+      하않존중: song.hajjCount,
+    })) ?? [];
+
+  if (!poll) return <Loading />;
 
   return (
     <DefaultLayout>
@@ -14,7 +27,7 @@ const VoteResult = () => {
         <header className={styles.header}>
           <section className={styles.vote_title}>
             <img src={ArrowBack} alt="뒤로가기" onClick={() => navigate(-1)} />
-            <h1>5월 대동제 곡 투표 결과</h1>
+            <h1>{poll?.title}</h1>
           </section>
           <section>
             <select className={styles.result_filter}>
