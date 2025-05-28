33b14736a97b94c49bcf924bf44f73cb9fc66571
Junmingeun <junminguen@naver.com>
2025-05-28 17:27:39 +0900

    feat: 페이지 response 타입 설정 변경

--- src/types/common.ts
+++ src/types/common.ts
@@ -1,40 +1,18 @@
 export type Nullable<T> = T | null;
 
-export interface PageNationResponse<T> {
+export interface PagiNationResponse<T> {
   content: T[];
+  pageInfo: PageInfo;
+}
+
+export interface PageInfo {
+  empty: boolean;
+  first: boolean;
+  last: boolean;
   page: number;
   size: number;
   totalElements: number;
   totalPages: number;
-  last: boolean;
-}
-
-export interface Sort {
-  empty: boolean;
-  sorted: boolean;
-  unsorted: boolean;
-}
-export interface Pageable {
-  pageNumber: number;
-  pageSize: number;
-  sort: Sort;
-  offset: number;
-  paged: boolean;
-  unpaged: boolean;
-}
-
-export interface PageableResponse<T> {
-  content: T[];
-  pageable: Pageable;
-  last: boolean;
-  totalElements: number;
-  totalPages: number;
-  size: number;
-  number: number;
-  sort: Sort;
-  first: boolean;
-  numberOfElements: number;
-  empty: boolean;
 }
 
 export type SetState<T> = React.Dispatch<React.SetStateAction<T>>;
