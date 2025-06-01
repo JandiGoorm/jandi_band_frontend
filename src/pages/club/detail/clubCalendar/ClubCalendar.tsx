faef80bea3a076f96729d86e22352e84de302982
S2ej1n <ks320205@naver.com>
2025-06-02 06:12:17 +0900

    design: 캘린더 디자인 수정(일정추가버튼)

--- src/pages/club/detail/clubCalendar/ClubCalendar.tsx
+++ src/pages/club/detail/clubCalendar/ClubCalendar.tsx
@@ -1,27 +1,10 @@
-import Button from "@/components/button/Button";
 import styles from "./ClubCalendar.module.css";
 import Calendar from "./calendar/Calendar";
-import Modal from "@/components/modal/Modal";
-import ScheduleModal from "./ScheduleModal";
 
-const ClubCalendar = ({ isMember }: { isMember: boolean }) => {
+const ClubCalendar = () => {
   return (
     <main className={styles.container}>
-      <header className={styles.header}>
-        {isMember && (
-          <Modal
-            title="일정 추가하기"
-            trigger={
-              <Button variant="primary" size="md">
-                일정 등록
-              </Button>
-            }
-          >
-            <ScheduleModal />
-          </Modal>
-        )}
-      </header>
-      <Calendar />
+      <Calendar isMember />
     </main>
   );
 };
