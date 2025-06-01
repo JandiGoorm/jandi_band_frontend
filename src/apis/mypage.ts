369ad835da3aae386dad9ce5f5d1791cee5d73b3
S2ej1n <ks320205@naver.com>
2025-06-02 02:54:30 +0900

    feature: 마이프로필 조회 api 연결

--- src/apis/mypage.ts
+++ src/apis/mypage.ts
@@ -1,10 +1,11 @@
 // 마이페이지 관련 api
 import { ApiEndpotins } from "@/constants/endpoints";
 import { useFetch, usePatch } from "./hooks";
+import type { MyInfo } from "@/types/mypage";
 
 // 내 정보 조회
 export const useGetInfo = () => {
-  return useFetch(ApiEndpotins.ME);
+  return useFetch<MyInfo>(ApiEndpotins.ME);
 };
 
 // 내 정보 수정
