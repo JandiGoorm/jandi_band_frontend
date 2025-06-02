7be85bdd9cbdc161219b3ebc5231b9c8f45d0fc2
JunMinGeun <80248856+Jun-min-geun@users.noreply.github.com>
2025-06-02 23:41:26 +0900

    feat: 홍보 detail 페이지 홍보물 가져오기 연결

--- src/pages/promotions/detail/DetailContent.tsx
+++ src/pages/promotions/detail/DetailContent.tsx
@@ -5,14 +5,21 @@ import { useNavigate, useParams } from "react-router-dom";
 import { buildPath } from "@/utils/buildPath";
 import { PageEndpoints } from "@/constants/endpoints";
 import DeleteModal from "@/components/modal/deleteModal/DeleteModal";
-import { useDeletePromo } from "@/apis/promotion";
+import { useDeletePromo, useGetPromo } from "@/apis/promotion";
+import Loading from "@/components/loading/Loading";
+import { formatPromotionDate } from "@/utils/dateStatus";
 
 const DetailContent = () => {
   const { id } = useParams();
   const navigate = useNavigate();
   const { mutate: deletePromo } = useDeletePromo(id!);
+  const { data: fetchData, isLoading: fetchLoading } = useGetPromo(id || "");
+
+  console.log(fetchData);
 
   if (!id) return;
+
+  if (!fetchData || fetchLoading) return <Loading />;
 
   return (
     <>
@@ -21,7 +28,7 @@ const DetailContent = () => {
           <span className={styles.promo_button}>공연 예정</span>
         </section>
         <div className={styles.title_box}>
-          <p className={styles.promo_title}>뫄 동아리 8월 정기 공연</p>
+          <p className={styles.promo_title}>{fetchData.data.title}</p>
           <div className={styles.title_button_box}>
             <Button
               size="sm"
@@ -46,11 +53,11 @@ const DetailContent = () => {
           </div>
         </div>
         <section className={styles.basic_info}>
-          <p>조회 20</p>
-          <p>댓글 20</p>
-          <p>좋아요 20</p>
-          <p>작성일 20</p>
-          <p>작성자 20</p>
+          <p>조회 {fetchData.data.viewCount}</p>
+          <p>댓글 {fetchData.data.commentCount}</p>
+          <p>좋아요 {fetchData.data.likeCount}</p>
+          <p>작성일 {formatPromotionDate(fetchData.data.createdAt)}</p>
+          <p>작성자 {fetchData.data.creatorName}</p>
         </section>
       </header>
       <section className={styles.promotion_container}>
@@ -67,23 +74,23 @@ const DetailContent = () => {
             <section className={styles.info_box}>
               <div className={styles.info}>
                 <p className={styles.info_title}>공연팀</p>
-                <p>아이유팀</p>
+                <p>{fetchData.data.teamName}</p>
               </div>
               <div className={styles.info}>
                 <p className={styles.info_title}>관람료</p>
-                <p>5000원</p>
+                <p>{fetchData.data.admissionFee}</p>
               </div>
               <div className={styles.info}>
                 <p className={styles.info_title}>날짜</p>
-                <p>2025년 3월 17일 월요일</p>
+                <p>{fetchData.data.eventDatetime}</p>
               </div>
               <div className={styles.info}>
                 <p className={styles.info_title}>시간</p>
-                <p>PM 6:00</p>
+                <p>{fetchData.data.eventDatetime}</p>
               </div>
               <div className={styles.info}>
                 <p className={styles.info_title}>장소</p>
-                <p>뫄뫄 공연장</p>
+                <p>{fetchData.data.location}</p>
                 <Button size="sm" variant="transparent">
                   지도보기
                 </Button>
@@ -91,10 +98,11 @@ const DetailContent = () => {
             </section>
           </section>
         </section>
-        <article className={styles.promo_content}>
-          안녕하세요 00대학교 동아리 아이유팀에서 70번째 정기 공연을 하게
-          되었습니다. 부원들이 열심히 준비한 무대이니 많은 관심 부탁드립니다.
-        </article>
+        {fetchData.data.description !== "" && (
+          <article className={styles.promo_content}>
+            {fetchData.data.description}
+          </article>
+        )}
       </section>
     </>
   );
