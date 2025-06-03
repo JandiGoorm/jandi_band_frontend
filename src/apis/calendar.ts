1ef0aef25311587c2b5d0dedee7d0aad1161f01c
S2ej1n <ks320205@naver.com>
2025-06-03 18:10:51 +0900

    feature: 캘린더 타입 추가

--- src/apis/calendar.ts
+++ src/apis/calendar.ts
@@ -1,1 +1,13 @@
 // 동아리 일정 관련 로직들
+import { ApiEndpotins } from "@/constants/endpoints";
+import { buildPath } from "@/utils/buildPath";
+import { useFetch } from "./hooks";
+import type { CalendarListType } from "@/types/calendar";
+
+// 캘린더 전체 일정 조회 GET - 응답형식 정의해야함.
+export const useGetPoll = (clubId: number, year: number, month: number) => {
+  const path = buildPath(ApiEndpotins.CALENDAR, { clubId });
+  const url = `${path}?year=${year}&month=${month}`;
+
+  return useFetch<CalendarListType>(url);
+};
