6cd4db622c8505fe1161ef13c5c214aaa494ee14
S2ej1n <ks320205@naver.com>
2025-06-03 16:25:38 +0900

    design: 달력 디자인 수정

--- src/pages/club/detail/clubCalendar/calendarLabel/data.ts
+++ src/pages/club/detail/clubCalendar/calendarLabel/data.ts
@@ -1,14 +1,14 @@
 export interface Schedule {
   date: string; // ex) "2025-06-04"
   name: string;
-  color: string; // ex) "#aaf4a2"
+  color: string;
 }
 
 export const schedules: Schedule[] = [
-  { date: "2025-06-04", name: "동아리일정", color: "#aaf4a2" },
-  { date: "2025-06-04", name: "합주일정", color: "#f6baff" },
-  { date: "2025-06-04", name: "합주일정", color: "#f6baff" },
-  { date: "2025-06-04", name: "합주일정", color: "#f6baff" },
-  { date: "2025-06-04", name: "합주일정", color: "#f6baff" },
-  { date: "2025-06-05", name: "회의..감이들어 우하하", color: "pink" },
+  { date: "2025-06-04", name: "동아리일정", color: "lightblue" },
+  { date: "2025-06-04", name: "합주일정", color: "pink" },
+  { date: "2025-06-04", name: "합주일정", color: "pink" },
+  { date: "2025-06-04", name: "합주일정", color: "pink" },
+  { date: "2025-06-04", name: "합주일정", color: "pink" },
+  { date: "2025-06-05", name: "회의..감이들어 우하하", color: "lightblue" },
 ];
