3f4f063ef179e78a7f7c95cbc8eb9e993ca08e98
S2ej1n <ks320205@naver.com>
2025-06-06 05:06:56 +0900

    feat: 동아리 일정 삭제 secure 추가

--- src/apis/secureRoutes.ts
+++ src/apis/secureRoutes.ts
@@ -49,4 +49,5 @@ export const secureRoutes = [
 
   { method: Method.GET, url: ApiEndpotins.CALENDAR },
   { method: Method.POST, url: ApiEndpotins.POST_CALENDAR_EVENT },
+  { method: Method.DELETE, url: ApiEndpotins.DELETE_CALENDAR_EVENT },
 ];
