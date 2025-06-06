2087bf31cff3a209abb2dc60140301b0ff2c4bb5
S2ej1n <ks320205@naver.com>
2025-06-07 04:22:19 +0900

    feat: 연습 일정 삭제 새로고침 구현

--- src/pages/team/detail/scheduleBoard/DeleteButton.tsx
+++ src/pages/team/detail/scheduleBoard/DeleteButton.tsx
@@ -2,14 +2,27 @@ import { useDeleteTeamSchedule } from "@/apis/calendar";
 import { useTeamStore } from "@/stores/teamStore";
 import styles from "./ScheduleBoard.module.css";
 
+import { ApiEndpotins } from "@/constants/endpoints";
+import { useQueryClient } from "@tanstack/react-query";
+import { buildPath } from "@/utils/buildPath";
+
+const size = 10;
+const page = 0;
+
 export const DeleteButton = ({ scheduleId }: { scheduleId: number }) => {
   const teamId = useTeamStore((state) => state.teamId);
   const { mutate: deleteEvent } = useDeleteTeamSchedule(teamId!, scheduleId);
+  const queryClient = useQueryClient();
 
   const handleDelete = () => {
     deleteEvent(undefined, {
       onSuccess: () => {
-        console.log("삭제 완료");
+        queryClient.invalidateQueries({
+          queryKey: [
+            buildPath(ApiEndpotins.TEAM_SCHEDULES, { teamId: teamId! }),
+            { page, size },
+          ] as const,
+        });
       },
     });
   };
