c207d17883e15c3fd2b78ea9edd455427057ef36
Junmingeun <junminguen@naver.com>
2025-06-06 17:35:30 +0900

    feat: 홍보물디테일페이지 지도보기 구현

--- src/pages/promotions/detail/LocationModal.tsx
+++ src/pages/promotions/detail/LocationModal.tsx
@@ -1,13 +1,24 @@
 import type { PromotionListResponse } from "@/types/promotion";
-
+import { Map, MapMarker } from "react-kakao-maps-sdk";
+import styles from "./LocationModal.module.css";
 const LocationModal = ({ data }: { data: PromotionListResponse }) => {
-  console.log(data);
+  const lat = data.latitude;
+  const lng = data.longitude;
+  console.log("위도/경도:", data.latitude, data.longitude);
   return (
-    <main>
-      <section></section>
-      <section>
-        <p>{data.location}</p>
-        <p>{data.address}</p>
+    <main className={styles.container}>
+      <section className={styles.map_box}>
+        <Map
+          center={{ lat, lng }}
+          style={{ width: "100%", height: "100%" }}
+          level={5}
+        >
+          <MapMarker position={{ lat, lng }} />
+        </Map>
+      </section>
+      <section className={styles.address_box}>
+        <p className={styles.location}>{data.location}</p>
+        <p className={styles.address}>{data.address}</p>
       </section>
     </main>
   );
