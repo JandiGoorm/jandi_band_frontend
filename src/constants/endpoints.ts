3e39ba297bb642cef6f0e9f036119cc7f225f96e
S2ej1n <ks320205@naver.com>
2025-06-07 03:58:24 +0900

    feat: 연습 일정 삭제 endpoint

--- src/constants/endpoints.ts
+++ src/constants/endpoints.ts
@@ -59,6 +59,6 @@ export enum ApiEndpotins {
   POST_CALENDAR_EVENT = "/clubs/:clubId/events", // 동아리 일정 추가
   DELETE_CALENDAR_EVENT = "/clubs/:clubId/events/:eventId", // 동아리 일정 삭제
   // 팀 연습 일정 등록 및 조회
-  TEAM_SCHEDULES = "/teams/:teamId/practice-schedules", // 뒤에 쿼리 파라미터 들어감
-  // 연습 일정 생성은 POST로 보내면됨
+  TEAM_SCHEDULES = "/teams/:teamId/practice-schedules",
+  DELETE_TEAM_SCHEDULES = "/teams/:teamId/practice-schedules/:scheduleId", // 팀 연습 일정 삭제
 }
