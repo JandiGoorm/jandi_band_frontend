670664ec56a92d379e9587512542b521b7e2074a
S2ej1n <ks320205@naver.com>
2025-06-07 01:49:18 +0900

    feat: 불참 포지션 버튼 생성 및 서버와 타입 일치

--- src/pages/team/detail/scheduleBoard/AddPractice.tsx
+++ src/pages/team/detail/scheduleBoard/AddPractice.tsx
@@ -17,6 +17,9 @@ const addFormSchema = z
     name: z.string().nonempty("캘린더에 표시될 일정 이름을 입력해주세요."),
     startDatetime: z.string().nonempty("시작 시간과 날짜를 선택하세요."),
     endtime: z.string().nonempty("종료 시간을 선택하세요."),
+    noPosition: z
+      .string()
+      .nonempty("불참 포지션을 선택해주세요. 없을 경우 없음을 선택해주세요."),
   })
   .refine(
     (data) => {
@@ -31,6 +34,17 @@ const addFormSchema = z
 
 // 폼 타입 추출
 export type AddFormData = z.infer<typeof addFormSchema>;
+
+// const positions = ["VOCAL", "GUITAR", "KEYBOARD", "BASS", "DRUM", "NULL"];
+
+const positions = {
+  NULL: "없음(전체참여)",
+  VOCAL: "보컬",
+  GUITAR: "기타",
+  KEYBOARD: "키보드",
+  BASS: "베이스",
+  DRUM: "드럼",
+} as const;
 
 export default function AddPractice({ setOpen }: Props) {
   // 스키마랑 연결
@@ -49,12 +63,14 @@ export default function AddPractice({ setOpen }: Props) {
   const onSubmit = (data: AddFormData) => {
     const start = new Date(data.startDatetime);
     const end = computeEndDatetime(data.startDatetime, data.endtime);
+    const finalNoPosition = data.noPosition === "NULL" ? null : data.noPosition;
 
     // 서버와 양식 일치
     const payload = {
       name: data.name,
       startDatetime: formatToServer(start),
       endDatetime: formatToServer(end),
+      noPosition: finalNoPosition,
     };
 
     console.log(payload);
@@ -69,7 +85,11 @@ export default function AddPractice({ setOpen }: Props) {
 
       <form className={styles.form_container} onSubmit={handleSubmit(onSubmit)}>
         <Field label="일정 이름" error={errors.name} isRequired>
-          <Input inputSize="sm" {...form.register("name")} />
+          <Input
+            inputSize="sm"
+            {...form.register("name")}
+            placeholder="예시: 새벽별 합주"
+          />
         </Field>
         <Field label="연습 시간" error={errors.startDatetime} isRequired>
           <Input
@@ -81,6 +101,26 @@ export default function AddPractice({ setOpen }: Props) {
         <Field label="연습 종료 시간" error={errors.endtime} isRequired>
           <Input inputSize="sm" type="time" {...form.register("endtime")} />
         </Field>
+
+        <Field label="불참 포지션" error={errors.noPosition} isRequired>
+          <div className={styles.radio_button_group}>
+            {Object.entries(positions).map(([key, label]) => (
+              <label key={key} className={styles.radio_button}>
+                <input
+                  type="radio"
+                  value={key}
+                  {...form.register("noPosition")}
+                  className={styles.radio_input}
+                />
+                <span>{label}</span>
+              </label>
+            ))}
+          </div>
+        </Field>
+        <p className={styles.guide}>
+          등록한 일정은 동아리 메인 캘린더에서도 확인이 가능합니다.
+        </p>
+
         <Button type="submit" size="md" variant="secondary">
           일정 등록하기
         </Button>
