7e0bd24bcff42c2c96571ff9f145caf95d29604d
S2ej1n <ks320205@naver.com>
2025-06-07 03:40:59 +0900

    feat: 일정 조회 api 연결

--- src/pages/team/detail/scheduleBoard/ScheduleBoard.tsx
+++ src/pages/team/detail/scheduleBoard/ScheduleBoard.tsx
@@ -1,22 +1,21 @@
-// import { useState } from "react";
+import { useState } from "react";
 import styles from "./ScheduleBoard.module.css";
 import { format } from "date-fns";
 import { FiPlus } from "react-icons/fi";
 import Modal from "@/components/modal/Modal";
-import { dummyTeamSchedule } from "./dummyTeamSchedule";
 import AddPractice from "./AddPractice";
-// import { useGetTeamSchedules } from "@/apis/calendar"
-// import { useTeamStore } from "@/stores/teamStore";
+import { useGetTeamSchedules } from "@/apis/calendar";
+import { useTeamStore } from "@/stores/teamStore";
 
-// const size = 10;
+const size = 10;
 
 const ScheduleBoard = () => {
-  // const teamId = useTeamStore((state) => state.teamId);
-  // const [page, setPage] = useState(0);
-  // const { data } = useGetTeamSchedules(teamId!, page, size);
+  const teamId = useTeamStore((state) => state.teamId);
+  const [page, setPage] = useState(0);
+  const { data } = useGetTeamSchedules(teamId!, page, size);
 
-  // const schedules = data?.data.content ?? [];
-  // const pageInfo = data?.data.pageInfo;
+  const schedules = data?.data.content ?? [];
+  const pageInfo = data?.data.pageInfo;
 
   return (
     <section className={styles.container}>
@@ -35,19 +34,44 @@ const ScheduleBoard = () => {
       </header>
 
       {/* 연습일정 */}
-      <div className={styles.content_container}>
-        {dummyTeamSchedule.map((item) => (
-          <div className={styles.content_item} key={item.id}>
-            <span className={styles.content_type}>
-              {item.noPosition !== null ? `NO ${item.noPosition}` : "전체"}
-            </span>
-            <div className={styles.content_item_info}>
-              <span>{item.name}</span>
-              <p>{`${format(new Date(item.startDatetime), "MM월 dd일 HH:mm")} ~ ${format(new Date(item.endDatetime), "HH:mm")}`}</p>
-            </div>
+      {pageInfo?.totalElements === 0 ? (
+        <p className={styles.empty_message}>
+          연습 일정이 없습니다. <br />
+        </p>
+      ) : (
+        <>
+          <div className={styles.content_container}>
+            {schedules.map((item) => (
+              <div className={styles.content_item} key={item.id}>
+                <span className={styles.content_type}>
+                  {item.noPosition !== null ? `NO ${item.noPosition}` : "전체"}
+                </span>
+                <div className={styles.content_item_info}>
+                  <span>{item.name}</span>
+                  <p>{`${format(new Date(item.startDatetime), "MM월 dd일 HH:mm")} ~ ${format(new Date(item.endDatetime), "HH:mm")}`}</p>
+                </div>
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
+          <div className={styles.pagination}>
+            <button
+              onClick={() => setPage((prev) => prev - 1)}
+              disabled={pageInfo?.first}
+            >
+              이전
+            </button>
+            <span>
+              {page + 1} / {pageInfo?.totalPages}
+            </span>
+            <button
+              onClick={() => setPage((prev) => prev + 1)}
+              disabled={pageInfo?.last}
+            >
+              다음
+            </button>
+          </div>
+        </>
+      )}
     </section>
   );
 };
