66ac4ae0e02d1ca0ab458e778de0adf91a0c076d
JunMinGeun <80248856+Jun-min-geun@users.noreply.github.com>
2025-06-07 13:15:03 +0900

    chore: 색 오류 수정

--- src/utils/dateStatus.ts
+++ src/utils/dateStatus.ts
@@ -8,9 +8,9 @@ export const getVoteStatus = (endDatetime: Date): VoteStatus => {
   const endDate = new Date(endDatetime);
 
   if (endDate > now) {
-    return { text: "투표중", backgroundColor: "var(--color-bg-toast-success)" }; // 초록색 예시
+    return { text: "투표중", backgroundColor: "var(--color-bg-toast-success)" };
   } else {
-    return { text: "투표마감", backgroundColor: "var(-color-bg-toast-error)" }; // 빨간색 예시
+    return { text: "투표마감", backgroundColor: "var(--color-error)" };
   }
 };
 
