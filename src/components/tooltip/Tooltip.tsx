be1ccac39eb256f2771e49a56fcea236b51db910
Junmingeun <junminguen@naver.com>
2025-06-09 19:23:02 +0900

    feat: tooltip 컴포넌트 생성

---
+++ src/components/tooltip/Tooltip.tsx
@@ -0,0 +1,31 @@
+import * as RadixTooltip from "@radix-ui/react-tooltip";
+import styles from "./Tooltip.module.css";
+
+interface TooltipProps {
+  trigger: React.ReactNode;
+  description: string;
+}
+
+const Tooltip: React.FC<TooltipProps> = ({ trigger, description }) => {
+  return (
+    <RadixTooltip.Provider delayDuration={200}>
+      <RadixTooltip.Root>
+        <RadixTooltip.Trigger asChild>
+          <button>{trigger}</button>
+        </RadixTooltip.Trigger>
+        <RadixTooltip.Portal>
+          <RadixTooltip.Content
+            className={styles.tooltip_content}
+            side="top"
+            sideOffset={4}
+          >
+            {description}
+            <RadixTooltip.Arrow className={styles.tooltip_arrow} />
+          </RadixTooltip.Content>
+        </RadixTooltip.Portal>
+      </RadixTooltip.Root>
+    </RadixTooltip.Provider>
+  );
+};
+
+export default Tooltip;
