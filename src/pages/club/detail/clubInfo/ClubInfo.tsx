3718233cfa10fd0a1daaaa8b67b23eed072eee49
JunMinGeun <80248856+Jun-min-geun@users.noreply.github.com>
2025-06-16 03:58:19 +0900

    fix: 병합 오류 수정

--- src/pages/club/detail/clubInfo/ClubInfo.tsx
+++ src/pages/club/detail/clubInfo/ClubInfo.tsx
@@ -75,8 +75,7 @@ const ClubInfo = ({
         </div>
 
         <div className={styles.left_title}>
-          
-          {isMember && <InviteModal />}
+          {isMember && <InviteModal nameValue={club.name} />}
 
           {mine && (
             <>
