06c5ca5939b4bd8e3f3f1d5380da5b60a8daa53d
JunMinGeun <80248856+Jun-min-geun@users.noreply.github.com>
2025-06-16 04:23:44 +0900

    feat: Inter 폰트 설정

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./reset.css";
 import "./variables.css";
+import "./global.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
