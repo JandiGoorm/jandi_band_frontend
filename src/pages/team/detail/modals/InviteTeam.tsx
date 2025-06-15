782f90301591a852e0dddbb83a4e6dae79324294
S2ej1n <ks320205@naver.com>
2025-06-15 23:40:29 +0900

    comment: 초대코드 모달 부분 주석 추가

--- src/pages/team/detail/modals/InviteTeam.tsx
+++ src/pages/team/detail/modals/InviteTeam.tsx
@@ -1,12 +1,13 @@
-import InviteModal from "@/components/modal/inviteModal/InviteModal";
+// 이건 팀 내의 invite 모달
 import { useParams } from "react-router-dom";
 import { useInviteTeam } from "@/apis/team";
+import InviteModal from "@/components/modal/inviteModal/InviteModal";
 
 const InviteTeam = () => {
   const { id } = useParams();
   const { data, mutate } = useInviteTeam(id ?? "");
 
-  return <InviteModal type="team" data={data} mutate={mutate} />;
+  return <InviteModal data={data} mutate={mutate} type="team" />;
 };
 
 export default InviteTeam;
