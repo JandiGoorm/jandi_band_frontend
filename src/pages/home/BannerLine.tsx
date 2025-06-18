77ec5057c744f0f7d80bb0acd7f47fa12553f591
S2ej1n <ks320205@naver.com>
2025-06-18 21:01:07 +0900

    refactor: 가운데 라벨 배너로 보이게 수정

--- src/pages/home/BannerLine.tsx
+++ src/pages/home/BannerLine.tsx
@@ -4,14 +4,14 @@ import styles from "./BannerLine.module.css";
 
 export default function BannerLine() {
   return (
-    <section className={styles.centered}>
+    <section className={styles.banner_section}>
       <div className={styles.line_box}>
         <p className={styles.line_title}>
           모두가 기다리는 그 밴드, 바로 여러분일지도!
         </p>
         <CreateClubModal
           trigger={
-            <Button variant="transparent" size="sm">
+            <Button variant="transparent" size="sm" className={styles.button}>
               동아리 만들기
             </Button>
           }
