8575bb46337835fdaaed44fb352b925fe2e7a0ec
S2ej1n <ks320205@naver.com>
2025-06-18 21:16:16 +0900

    refactor: 밴드 무대 타이틀 줄바꿈

--- src/pages/home/PromotionSlide.tsx
+++ src/pages/home/PromotionSlide.tsx
@@ -4,18 +4,28 @@ import type { PromotionListResponse } from "@/types/promotion";
 import { useNavigate } from "react-router-dom";
 import { buildPath } from "@/utils/buildPath";
 import { PageEndpoints } from "@/constants/endpoints";
+import { useEffect, useState } from "react";
 
 const PromotionSlide = ({ promo }: { promo: PromotionListResponse[] }) => {
   const navigate = useNavigate();
 
-  console.log(promo);
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => setWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <main className={styles.main_container}>
       <p
         className={styles.title}
         onClick={() => navigate(PageEndpoints.PROMOTION)}
       >
-        현장을 달굴 밴드들의 무대, 지금 바로 확인하세요!
+        현장을 달굴 밴드들의 무대,
+        {width < 500 ? <br /> : " "}
+        지금 바로 확인하세요!
       </p>
       <MainSlide<PromotionListResponse> items={promo}>
         {(item) => (
